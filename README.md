551cfcf12330a3f09ea49ed53c54299138d2e9d1
loicpennequin <lo.pennequin@gmail.com>
2022-11-24 18:31:51 +0100

    initialize db package

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# Fun with socket.io and canvas
+# TODO
+
+## Prerequisites
+
+- node >= 16
+- docker and docker-compose
 
 ## Get Started
 
 - `yarn install`
 - `yarn dev`
-
-or you can open 2 terminals separately and tun
-
-- `yarn server dev`
-- `yarn client dev`
-
-Have fun !
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,13 +1,14 @@
 import { type inferAsyncReturnType, initTRPC } from '@trpc/server';
 import type http from 'http';
+import { prisma } from '@dotz/db';
 
-export const t = initTRPC.create();
+export const t = initTRPC.context<Context>().create();
 export const apiRouter = t.router({
-  hello: t.procedure.query(() => {
-    return 'hello from trpc with nodemon update';
+  hello: t.procedure.query(({ ctx }) => {
+    return ctx.db.user.findMany();
   })
 });
-// export type definition of API
+
 export type ApiRouter = typeof apiRouter;
 
 type CreateContextOptions = {
@@ -16,7 +17,8 @@ type CreateContextOptions = {
 };
 export const createApiContext = ({ req, res }: CreateContextOptions) => ({
   req,
-  res
+  res,
+  db: prisma
 });
 
 export type Context = inferAsyncReturnType<typeof createApiContext>;
